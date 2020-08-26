3e543655661cc995421262296f2ba060f1fd623e
John Riviere <mr.jriviere@gmail.com>
2020-08-26 10:22:17 -0500

    can check everything

--- client.js
+++ client.js
@@ -1,34 +1,34 @@
-const employees = [
-  {
-    name: 'Atticus',
-    employeeNumber: '2405',
-    annualSalary: '47000',
-    reviewRating: 3
-  },
-  {
-    name: 'Jem',
-    employeeNumber: '62347',
-    annualSalary: '63500',
-    reviewRating: 4
-  },
-  {
-    name: 'Scout',
-    employeeNumber: '6243',
-    annualSalary: '74750',
-    reviewRating: 5
-  },
-  {
-    name: 'Robert',
-    employeeNumber: '26835',
-    annualSalary: '66000',
-    reviewRating: 1
-  },
-  {
-    name: 'Mayella',
-    employeeNumber: '89068',
-    annualSalary: '35000',
-    reviewRating: 1
-  }
+const employees = [{
+
+    name: "Atticus",
+    employeeNumber: "2405",
+    annualSalary: "47000",
+    reviewRating: 3,
+},
+{
+    name: "Jem",
+    employeeNumber: "62347",
+    annualSalary: "63500",
+    reviewRating: 4,
+},
+{
+    name: "Scout",
+    employeeNumber: "6243",
+    annualSalary: "74750",
+    reviewRating: 5,
+},
+{
+    name: "Robert",
+    employeeNumber: "26835",
+    annualSalary: "66000",
+    reviewRating: 1,
+},
+{
+    name: "Mayella",
+    employeeNumber: "89068",
+    annualSalary: "95000",
+    reviewRating: 1,
+},
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
@@ -40,4 +40,91 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+console.log(employees);
+
+/*## Processing Employee Bonuses
+
+Loop over the `employees` array and do the following:
+
+* use each employee object as the input to the function described below.
+* `console.log` the results of each iteration.
+
+## Function Logic
+
+Write a declared function that takes in one **Employee** object (as an argument to the function), and returns a new **object** with the following properties:
+
+* The `name` property should contain the employee's name.
+* The `bonusPercentage` property should contain the bonus percentage the employee is to receive. See section below for calculation instructions.
+* The `totalCompensation` property should be the adjusted annual compensation (base annual + bonus)
+* The `totalBonus` should be the employee's total bonus rounded to the nearest dollar.
+
+- Those who have a rating of a 2 or below should not receive a bonus.
+- Those who have a rating of a 3 should receive a base bonus of 4% of their base annual income.
+- Those who have a rating of a 4 should receive a base bonus of 6% of their base annual income.
+- Those who have a rating of a 5 should receive a base bonus of 10% of their base annual income.
+- If their employee number is 4 digits long, this means they have been with the company for longer than 15 years,
+  and should receive an additional 5%.
+
+employeeNumber.length>4 then they get the bonus
+
+- However, if their annual income is greater than \$65,000, they should have their bonus adjusted down 1%.
+- No bonus can be above 13% or below 0% total.
+
+NOTE: You may abstract out this bonus calculation into a second function if you like, but this is not mandatory.
+*/
+
+function bonusCalculation(employees) {
+    for (let i = 0; i < employees.length; i++) {
+        console.log(employees[i]);
+        calculateBonusPercentage(employees[i]);
+    }
+};
+
+bonusCalculation(employees);
+//console.log(employees[i]);
+//}
+/*
+bonusCalculation({
+    name: "Mayella",
+    employeeNumber: "89068",
+    annualSalary: "35000",
+    reviewRating: 1,
+});
+*/
+
+
+function calculateBonusPercentage(object) {
+    const maxBonusPercent = .13;
+    const minBonusPercent = 0;
+    let bonusPercent = 0;
+    if (object.employeeNumber < 10000) {
+        bonusPercent = 0.05;
+        console.log(bonusPercent);
+    }
+    if (object.annualSalary > 65000) {
+        bonusPercent -= .01;
+        //console.log();
+    }
+
+
+    if (object.reviewRating < 3) {
+        console.log(object.name + ' no bonus ');
+    } else if (object.reviewRating === 3) {
+        bonusPercent += .04;
+        console.log(bonusPercent);
+        console.log(object.name + ' 4% bonus ');
+    } else if (object.reviewRating === 4) {
+        bonusPercent += .06;
+        console.log(bonusPercent);
+        console.log(object.name + ' 6% bonus ');
+    } else if (object.reviewRating === 5) {
+        bonusPercent += .10;
+        console.log(bonusPercent);
+        console.log(object.name + ' 10% bonus ');
+    }
+    if (bonusPercent > maxBonusPercent) {
+        bonusPercent = 0.13;
+    } else if (bonusPercent < minBonusPercent) {
+        bonusPercent = 0;
+    }
+}
